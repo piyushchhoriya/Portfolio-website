631cacf56f22fc828b9d239ef099cebe58af77b3
Piyush Chhoriya <piyushchhoriya98@gmial.com>
2025-03-07 01:17:17 -0500

    Added gh-pages setup for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'; 
+import react from '@vitejs/plugin-react'; // Import react plugin
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-website/', // Add this line to set the base URL for GitHub Pages
+  base: '/Portfolio-website/', // Adjust this to your GitHub repository name
   build: {
-    // Increase the chunk size warning limit (in KB)
-    chunkSizeWarningLimit: 1000, // Adjust as per your requirement
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // Optional: Customize how chunks are split
           if (id.includes('node_modules')) {
-            return 'vendor'; // Create a vendor chunk for third-party libraries
+            return 'vendor';
           }
         },
       },
